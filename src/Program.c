e01c834cb09eac28bc9ada3dda05eac6861daad8
jppacheaco <pacheaj@wwu.edu>
2022-06-21 13:42:05 -0700

    first crack at code

--- src/Program.c
+++ src/Program.c
@@ -8,5 +8,20 @@ Assignment: Lab 1
 
 int main () //use int if we are returning an int
 {
+    //get input from user
+    char input [];
+    cout << "Enter the number you'd like to convert: " << input;
+
+    int size = sizeof(input);
+    int output [size];
+
+    for(int i = 0; i<size; ++i){
+        int var = (int)(input[i]);
+        //convert
+        //add to output
+    }
+
+    //merge output array
+    //output final binary code
 
 }
