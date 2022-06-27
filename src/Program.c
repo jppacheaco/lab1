338b9ed32698dc9238a246b72179173f1b033378
jppacheaco <pacheaj@wwu.edu>
2022-06-27 14:22:51 -0700

    program almost works, need to fix issue where math is done using ascii

--- src/Program.c
+++ src/Program.c
@@ -1,27 +1,37 @@
-/**name: J.P. Pacheaco
-Assignment: Lab 1
+/*
+# Name: J.P. Pacheaco
+# Description: Converts prompted binary to a a decimal
+# Date:
+# Specification: Input is exactly 4 characters
 */
 
 //this "program" file is the main file
 
 #include <stdio.h>
-
-int main () //use int if we are returning an int
+/*
+Precondition: the inputted string is 4 characters of 1 or 0
+*/
+int main ()
 {
     //get input from user
-    char input [];
-    cout << "Enter the number you'd like to convert: " << input;
+    char input[4];
+    printf("Enter a four-bit binary number as a string: ");
+    gets(input);
 
-    int size = sizeof(input);
-    int output [size];
+    int total = 0;
 
-    for(int i = 0; i<size; ++i){
-        int var = (int)(input[i]);
-        //convert
-        //add to output
+    //convert to binary
+    int i;
+    for(i = 0; i < 4; ++i){
+        int var = input[i];
+        total = (total*2) + var;
     }
 
-    //merge output array
-    //output final binary code
+    //return output
+    printf("Binary Value: %s", input);
+    printf("    Decimal Value: %d", total);
 
+    return 0;
+
+    //QUESTION: HOW DO I STOP DOING MATH ON ASCII AND DO MATH ON CHARS INSTEAD
 }
