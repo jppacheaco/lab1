7867ed65f261000e4669ef44972a25d35d21602a
jppacheaco <pacheaj@wwu.edu>
2022-06-29 12:15:02 -0700

    The program runs properly. Currently subtracting 48 to account for ascii. might need to fix this.

--- src/Program.c
+++ src/Program.c
@@ -1,37 +1,36 @@
 /*
 # Name: J.P. Pacheaco
-# Description: Converts prompted binary to a a decimal
+# Description: Converts prompted binary to it's decimal.
 # Date:
-# Specification: Input is exactly 4 characters
+# Specification: Input is exactly 4 characters.
 */
 
-//this "program" file is the main file
-
-#include <stdio.h>
 /*
 Precondition: the inputted string is 4 characters of 1 or 0
 */
 int main ()
 {
+    #include<stdio.h>
+    #include<stdlib.h>
+    #include<string.h>
+
     //get input from user
     char input[4];
     printf("Enter a four-bit binary number as a string: ");
-    gets(input);
+    scanf("%s", input);
 
     int total = 0;
 
     //convert to binary
-    int i;
-    for(i = 0; i < 4; ++i){
-        int var = input[i];
-        total = (total*2) + var;
-    }
+
+    for(int i = 0; i < 4; ++i){
+        //subtracting 48 from value stored in input to account for ascii code
+        total = (total*2) + (input[i] - 48);
+    }  
 
     //return output
     printf("Binary Value: %s", input);
-    printf("    Decimal Value: %d", total);
+    printf("    Decimal Value: %d\n", total);
 
     return 0;
-
-    //QUESTION: HOW DO I STOP DOING MATH ON ASCII AND DO MATH ON CHARS INSTEAD
 }
